6bb1d0c8bd6436e0bad7f84ec9b9e710cf010850
Kevin Borling <kborling@gmail.com>
2016-02-19 19:21:03 -0500

    Added CSS Minification to Style.css

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,82 +11,87 @@ module.exports = function(grunt) {
 			files: ['sass/**/*.scss'],
 			tasks: 'sass:dev',
 			options: {
-				livereload: true,
-			},
+				livereload: true
+			}
 		},
 		sass: {
 			dev: {
-		  		options : {
-			  		style : 'expanded'
-			  	},
-			  	files: {
-					'style.css':'sass/style.scss',
+	  		options : {
+		  		style : 'expanded'
+		  	},
+		  	files: {
+				'css/style.css':'sass/style.scss'
 				}
 			},
 			release: {
-		  		options : {
-			  		style : 'expanded'
-			  	},
-			  	files: {
-					'style.css':'sass/style.scss',
+	  		options : {
+		  		style : 'expanded'
+		  	},
+		  	files: {
+				'css/style.css':'sass/style.scss'
 				}
 			},
 		},
 		autoprefixer: {
-            options: {
+      options: {
 				browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1', 'ie 9']
 			},
 			single_file: {
-				src: 'style.css',
-				dest: 'style.css'
+				src: 'css/style.css',
+				dest: 'css/style.css'
 			}
 		},
 		csscomb: {
 			options: {
-                config: '.csscomb.json'
-            },
-            files: {
-                'style.css': ['style.css'],
-            }
+        config: '.csscomb.json'
+      },
+      files: {
+        'css/style.css': ['css/style.css']
+      }
+		},
+		cssmin: {
+			release: {
+				files: {
+	 				'css/style.min.css': ['css/style.css']
+				}
+			}
 		},
 		concat: {
-		    release: {
-		        src: [
-		            'js/skip-link-focus-fix.js',
-		            'js/navigation.js',
-		        ],
-		        dest: 'js/combined.min.js',
-		    }
+	    release: {
+        src: [
+          'js/skip-link-focus-fix.js',
+          'js/navigation.js'
+        ],
+        dest: 'js/combined.min.js'
+	    }
 		},
 		uglify: {
-		    release: {
-		        src: 'js/combined.min.js',
-		        dest: 'js/combined.min.js'
-		    }
-		},
-    	// https://www.npmjs.org/package/grunt-wp-i18n
-	    makepot: {
-	        target: {
-	            options: {
-	                domainPath: '/languages/',    // Where to save the POT file.
-	                potFilename: '_s.pot',   // Name of the POT file.
-	                type: 'wp-theme'  // Type of project (wp-plugin or wp-theme).
-	            }
-	        }
+	    release: {
+        src: 'js/combined.min.js',
+        dest: 'js/combined.min.js'
 	    }
-
+		},
+  	// https://www.npmjs.org/package/grunt-wp-i18n
+    makepot: {
+      target: {
+        options: {
+          domainPath: '/languages/',    // Where to save the POT file.
+          potFilename: '_s.pot',   // Name of the POT file.
+          type: 'wp-theme'  // Type of project (wp-plugin or wp-theme).
+        }
+      }
+    }
 	});
-
-    grunt.registerTask( 'default', [
-    	'sass:dev'
-    ]);
-    grunt.registerTask( 'release', [
-    	'sass:release',
+  grunt.registerTask( 'default', [
+  	'sass:dev'
+  ]);
+  grunt.registerTask( 'release', [
+  	'sass:release',
 		'autoprefixer',
 		'csscomb',
+		'cssmin:release',
 		'concat:release',
 		'uglify:release',
 		'makepot'
 	]);
-
 };
